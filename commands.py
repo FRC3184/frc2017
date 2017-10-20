557e114d56ea8ce62db823d705d821567906334c
Nick Schatz <nickbrickmaster@gmail.com>
2017-10-19 22:07:58 -0500

    Clean up radius drive code

--- commands.py
+++ commands.py
@@ -249,6 +249,7 @@ class OpDriveCommand(Command):
         qt_button = 2
         if js_left.getRawButton(spenner_button) or js_right.getRawButton(spenner_button):
             spenner = 0.7
+
         if js_left.getRawButton(tank_button) or js_right.getRawButton(tank_button):
             self.my_robot.drive.tankDrive(-spenner * js_left.getY(), -spenner * js_right.getY())
         elif js_left.getRawButton(qt_button) or js_right.getRawButton(qt_button):
@@ -256,7 +257,6 @@ class OpDriveCommand(Command):
         else:
             self.my_robot.drive.radius_drive(-mathutils.valpow(js_left.getY(), 2), mathutils.valpow(js_right.getX(), 2),
                                              spenner, velocity=self.velocity)
-            # self.my_robot.drive.arcadeDrive(-js_left.getY(), -js_right.getX())
 
 
 class DistanceDriveCommand(Command):
--- drivetrain.py
+++ drivetrain.py
@@ -11,24 +11,10 @@ class Drivetrain(wpilib.RobotDrive, Subsystem):
     def __init__(self, *args, **kwargs):
         wpilib.RobotDrive.__init__(self, *args)
         Subsystem.__init__(self)
-        if "robot_width" in kwargs.keys():
-            self.robot_width = kwargs['robot_width']
-        else:
-            self.robot_width = 29.25 / 12
-
-        if "max_radius" in kwargs.keys():
-            self.max_turn_radius = kwargs['max_radius']
-        else:
-            self.max_turn_radius = 10
-
-        if "wheel_diameter" in kwargs.keys():
-            self.wheel_diameter = kwargs['wheel_diameter']
-        else:
-            self.wheel_diameter = 4
-        if "max_speed" in kwargs.keys():
-            self.max_speed = kwargs['max_speed']
-        else:
-            self.max_speed = 13
+        self.robot_width = kwargs.pop("robot_width", 29.25 / 12)
+        self.max_turn_radius = kwargs.pop("max_radius", 10)
+        self.wheel_diameter = kwargs.pop("wheel_diameter", 4)
+        self.max_speed = kwargs.pop("max_speed", 13)
 
         self.ahrs = AHRS.create_i2c()
 
@@ -51,13 +37,12 @@ class Drivetrain(wpilib.RobotDrive, Subsystem):
             self.setLeftRightMotorOutputs(forward_power * power_factor, forward_power * power_factor, velocity=velocity)
             return
         if abs(forward_power) < 0.05:
-            # self.setLeftRightMotorOutputs(turn_power * power_factor, -turn_power * power_factor, velocity=velocity)
             self.setLeftRightMotorOutputs(0, 0, velocity=velocity)
             return
         turn_power = mathutils.valroot(turn_power, 3)
         radius = self.max_turn_radius * (1 - abs(turn_power))
         self.radius_turn(forward_power * power_factor,
-                         radius * mathutils.sgn(turn_power), # * mathutils.sgn(forward_power),
+                         radius * mathutils.sgn(turn_power),
                          velocity=velocity)
 
     def turn_to_angle(self, angle, allowable_error=2):
@@ -99,11 +84,20 @@ class Drivetrain(wpilib.RobotDrive, Subsystem):
 
     def setLeftRightMotorOutputs(self, leftOutput, rightOutput, velocity=False):
         if velocity:
-            ratio = 60 * self.max_speed / (math.pi * 4 * 12)  # max speed in rpm
+            ratio = self.get_fps_rev_ratio()  # max speed in rpm
             leftOutput *= ratio
             rightOutput *= ratio
+            if self.frontLeftMotor:
+                self.frontLeftMotor.set(leftOutput)
             self.rearLeftMotor.set(leftOutput)
-            self.rearRightMotor.set(-rightOutput)  # TODO make this apply to both motors
+            if self.frontRightMotor:
+                self.frontRightMotor.set(rightOutput)
+            self.rearRightMotor.set(-rightOutput)
+
+            self.feed()
         else:
             super().setLeftRightMotorOutputs(leftOutput, rightOutput)
+
+    def get_fps_rev_ratio(self):
+        return 60 * self.max_speed / (math.pi * 4 * 12)
 
